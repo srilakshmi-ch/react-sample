0a5351ddad22707c7a292fef7754bd4d717098bc
srilakshmi-ch <chitti.sri006@gmail.com>
2020-06-23 13:58:38 -0400

    Updates

---
+++ precache-manifest.6f1c44230da4f0345ee2e3360b991c7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e960f9824735fd704b2729b09fd41c",
+    "url": "/react-sample/index.html"
+  },
+  {
+    "revision": "0754ab09dbd39f2984a0",
+    "url": "/react-sample/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "69695ccf468145cf2315",
+    "url": "/react-sample/static/js/2.f7b4add9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sample/static/js/2.f7b4add9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0754ab09dbd39f2984a0",
+    "url": "/react-sample/static/js/main.eacbd16b.chunk.js"
+  },
+  {
+    "revision": "8c4893021d2801aa308e",
+    "url": "/react-sample/static/js/runtime-main.adfaf877.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-sample/static/media/logo.5d5d9eef.svg"
+  }
+]);
